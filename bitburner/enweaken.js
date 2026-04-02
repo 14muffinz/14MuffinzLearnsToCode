bd4600530309e79cfe673fe433c4ea331e557c3d
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-02 17:49:39 -0400

    Update enweaken.js

--- bitburner/enweaken.js
+++ bitburner/enweaken.js
@@ -1,14 +1,31 @@
-// at first i didn't know why this looped properly and another version of it did it.
-// i forgot the brackets. like, the most important thing in coding. ugh.
+// i... completely redid this code. I realised it was wasting time constantly weakening n00dles when it didn't need to
+// next up is definitely modifying masterclass becuase i just realised i'm a dunce who put everything in the 'else' when I really shouldn't have
 
 /** @param {NS} ns */
 export async function main(ns) {
-  while (true) {
-    await ns.weaken("n00dles");
-    await ns.weaken("foodnstuff");
-    await ns.weaken("sigma-cosmetics");
-    await ns.weaken("joesguns");
-    await ns.weaken("hong-fang-tea");
-    await ns.weaken("harakiri-sushi");
-  }
+     var target = "n00dles";
+     var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+     var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+     while (true) {
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+          }
+          await ns.hack(target)
+       // i might take out the hack step later, but it only hacks if it's already at the minimum weakness so... no harm no foul?
+       // i just realised it's actually not set up like that. new update coming soon
+          var target = "foodnstuff"
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+          }
+          await ns.hack(target)
+          var target = "sigma-cosmetics"
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+          }
+          await ns.hack(target)
+          var target = "joesguns"
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+          }
+     }
 }
