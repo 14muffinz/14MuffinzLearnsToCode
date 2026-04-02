aebd2b824b53e9dd9f68a56470e31d2aa0b0fed6
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-01 21:17:05 -0400

    Create enweaken.js

---
+++ enweaken.js
@@ -0,0 +1,13 @@
+// i'm not sure why this worked when the stuff on the laptop didn't, so I'm dumping it here until I can figure it out
+
+/** @param {NS} ns */
+export async function main(ns) {
+  while (true) {
+    await ns.weaken("n00dles");
+    await ns.weaken("foodnstuff");
+    await ns.weaken("sigma-cosmetics");
+    await ns.weaken("joesguns");
+    await ns.weaken("hong-fang-tea");
+    await ns.weaken("harakiri-sushi");
+  }
+}
