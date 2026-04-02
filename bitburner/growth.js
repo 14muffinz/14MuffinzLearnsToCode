34e4014e6138f797859037ca9d6eaa09903b46cd
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-01 23:56:41 -0400

    Create growth.js

---
+++ bitburner/growth.js
@@ -0,0 +1,20 @@
+// this one is for when you first unlock a specific server and need to grow it a lot. 
+export async function main(ns) {
+     let target = "foodnstuff";
+     let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+     let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+     while (true) {
+          if ((ns.getServerSecurityLevel(target) > securityThresh) && (ns.getServerMoneyAvailable(target) < moneyThresh)) {
+               await ns.grow(target);
+               await ns.weaken(target);
+               await ns.weaken(target);
+               await ns.weaken(target);
+          } else if ((ns.getServerSecurityLevel(target) <= securityThresh) && (ns.getServerMoneyAvailable(target) < moneyThresh)) {
+               await ns.grow(target)
+          } else if ((ns.getServerSecurityLevel(target) > securityThresh) && (ns.getServerMoneyAvailable(target) >= moneyThresh)) {
+               await ns.weaken(target);
+          } else {
+               await ns.hack(target);
+          }
+     }
+}
