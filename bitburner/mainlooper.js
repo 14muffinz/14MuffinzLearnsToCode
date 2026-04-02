96c2758d3ed7b2a84af385ac81321d0360faef4c
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-01 21:18:57 -0400

    Create mainlooper.js

---
+++ bitburner/mainlooper.js
@@ -0,0 +1,18 @@
+// this is code completely ripped off of google that automatically handles one hack at a time
+
+export async function main(ns) {
+    let target = "foodnstuff"; 
+    let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    while(true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
+}
+
+// this all loops as intended. my attempts to recreate/extend it... not so much.
