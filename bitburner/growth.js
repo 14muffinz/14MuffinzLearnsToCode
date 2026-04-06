2f9341d0371bb693c7809e8086d33d527de3a49c
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-06 18:02:45 -0400

    Update growth.js

--- bitburner/growth.js
+++ bitburner/growth.js
@@ -1,20 +1,47 @@
-// this one is for when you first unlock a specific server and need to grow it a lot. 
+// yeah the old code was weird. here's one that grows everything (as a parallel to enweaken)
 export async function main(ns) {
-     let target = "foodnstuff";
-     let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-     let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+     var target = "n00dles";
+     var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
      while (true) {
-          if ((ns.getServerSecurityLevel(target) > securityThresh) && (ns.getServerMoneyAvailable(target) < moneyThresh)) {
+          var target = "n00dles";
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
                await ns.grow(target);
-               await ns.weaken(target);
-               await ns.weaken(target);
-               await ns.weaken(target);
-          } else if ((ns.getServerSecurityLevel(target) <= securityThresh) && (ns.getServerMoneyAvailable(target) < moneyThresh)) {
-               await ns.grow(target)
-          } else if ((ns.getServerSecurityLevel(target) > securityThresh) && (ns.getServerMoneyAvailable(target) >= moneyThresh)) {
-               await ns.weaken(target);
+               await ns.sleep(500)
           } else {
-               await ns.hack(target);
-          } await ns.sleep(1000)
+               await ns.hack(target)
+          }
+          var target = "foodnstuff"
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
+               await ns.grow(target)
+               await ns.sleep(500)
+          } else {
+               await ns.hack(target)
+          }
+          var target = "sigma-cosmetics"
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
+               await ns.grow(target);
+               await ns.sleep(500)
+          } else {
+               await ns.hack(target)
+          }
+          var target = "joesguns"
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
+               await ns.grow(target);
+               await ns.sleep(500)
+          } else {
+               await ns.hack(target)
+          }
+          var target = "hong-fang-tea"
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
+               await ns.grow(target);
+               await ns.sleep(500)
+          } else {
+               await ns.hack(target)
+          }
+          var target = "harakiri-sushi"
+          if (ns.getServerMoneyAvailable(target) != ns.getServerMaxMoney(target)) {
+               await ns.grow(target);
+               await ns.sleep(500)
+          }
      }
 }
