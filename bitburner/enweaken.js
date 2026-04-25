333237b74078ae27ad855a9c016640b50d8c2f8e
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-25 16:12:20 -0400

    Update enweaken.js

--- bitburner/enweaken.js
+++ bitburner/enweaken.js
@@ -1,47 +1,53 @@
-// i... completely redid this code. I realised it was wasting time constantly weakening n00dles when it didn't need to
-// next up is definitely modifying masterclass becuase i just realised i'm a dunce who put everything in the 'else' when I really shouldn't have
+//i only just realised how out of date this code is, lol
 
-/** @param {NS} ns */
 /** @param {NS} ns */
 export async function main(ns) {
      var target = "n00dles";
      var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
      var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
      while (true) {
+          var target = "n00dles";
+          if (ns.getServerSecurityLevel(target) >= ns.getServerMinSecurityLevel(target) + .050) {
+               await ns.weaken(target);
+               await ns.sleep(1000)
+          } else {
+               await ns.hack(target)
+               await ns.sleep(1000)
+          }
+          var target = "nectar-net"
           if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
                await ns.weaken(target);
+               await ns.sleep(1000)
           } else {
-               await ns.hack(target)
+               await ns.sleep(1000)
           }
-          var target = "foodnstuff"
-          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
-               await ns.weaken(target) //embarassingly, i forgot  this line before.
-          } else {
-               await ns.hack(target)
-          }
-          var target = "sigma-cosmetics"
-          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
-               await ns.weaken(target);
-          } else {
-               await ns.hack(target)
-          }
-          var target = "joesguns"
-          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
-               await ns.weaken(target);
-          } else {
-               await ns.hack(target)
-          }
-          // finally put these into the template
-          // much easier to remove than whatever is happening in masterclass. thank goodness!
           var target = "hong-fang-tea"
           if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
                await ns.weaken(target);
+               await ns.sleep(1000)
           } else {
-               await ns.hack(target)
+               await ns.sleep(1000)
           }
           var target = "harakiri-sushi"
           if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
                await ns.weaken(target);
-          } await ns.sleep(1000)
+               await ns.sleep(1000)
+          } else {
+               await ns.sleep(1000)
+          }
+          var target = "zer0"
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+               await ns.sleep(1000)
+          } else {
+               await ns.sleep(1000)
+          }
+          var target = "phantasy"
+          if (ns.getServerSecurityLevel(target) != ns.getServerMinSecurityLevel(target)) {
+               await ns.weaken(target);
+               await ns.sleep(1000)
+          } else {
+               await ns.sleep(1000)
+          }
      }
 }
