b1a898f3caf64c71d2ebb29c4d09a9e1aa3b5407
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-30 22:57:35 -0400

    Update n00dles.js

--- bitburner/n00dles.js
+++ bitburner/n00dles.js
@@ -1,11 +1,13 @@
 // i figured that since n00dles is constantly weak, it needed its own loop
+// what it does need is a LOT of growing. might need to make a second vers for home
 export async function main(ns) {
      while (true) {
           if (ns.getServerSecurityLevel("n00dles") >= 1.050) {
                await ns.weaken("n00dles");
+          } else if(ns.getServerMoneyAvailable != 1750000) {
+               await ns.grow("n00dles")
           } else {
                await ns.hack("n00dles")
-          } await ns.sleep(1000)
+          } await ns.sleep(500)
      }
 }
-// breaking news, local idiot forgot to delete "min" and ruined his entire code
